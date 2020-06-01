51c546db430e620e93e8d939a4f6659ab667dca7
moulergu <remi.mouelrgues@laposte.net>
2020-06-01 22:28:26 +0000

    Bug

--- application/views/EleveQuizz.php
+++ application/views/EleveQuizz.php
@@ -7,8 +7,6 @@
 <body>
 <div class="header">
 <h1 align="center">Votre Quizz</h1>
-<h1>Votre Quizz</h1>
-
 </div><br><br>
 
 <!-- Cette page est l'affiche du Quizz donc affichage des questions,réponses. La page permet à l'utilisateur de répondre au quizz et valider ses réponses-->
@@ -29,8 +27,9 @@
 		$numeroQuestion[] = $TableQuestion[$i]['Numéro'];
 		$nombreRéponse[] = $TableQuestion[$i]['NombreRéponse'];
 
-	
-	$ID_connect=54;
+		}
+	}
+
 
 	for($i=0;$i<= count($resultat)-1;$i++){            /*Cette première boucle permet de récuperer les questions de la db et uniquement les questions qui possède le 														 même "ID"*/
 
@@ -93,24 +92,6 @@
 
 ?>
 	<hr><br>
-			$data2=$nombre[$i]['NombreQuestion'];
-		}
-	}
-		
-	/*Ici la boucle permet d'afficher les questions et réponses dans la page*/
-	for($i=0; $i < $data2; $i++){
-		?><h3 align="center" style="border: 1px solid #C82C3E; padding: 3px; background-color: #22C671;"> <?php echo $data[$i]?></h3> 
-		<form>
-		<fieldset>
-
-			<label>
-				<input type="radio" name="reponse<?php $i+1 ?>"> Oui</label>
-			<label>
-				<input type="radio" name="reponse<?php $i+1 ?>"> Non</label><br><br><br>
-			
-
-
-	<?php } ?>
 	
 	<div align="center">
 	<button type="submit">Envoyé</button>
